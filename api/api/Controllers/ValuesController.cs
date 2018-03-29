42fc4f25b87384de74488e44082e212bd6c181b0
段兵宇 <duanbingyu@52yunxi.cn>
2018-03-29 23:42:43 +0800

    xx

---
+++ api/api/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using api.Model;
+using Microsoft.Extensions.Options;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IOptions<WechatConfig> wechatconfig;
+        private readonly string appid;
+        private readonly string appsecret;
+        public ValuesController(IOptions<WechatConfig> settings)
+        {
+            wechatconfig = settings;
+            appid = wechatconfig.Value.appid;
+            appsecret = wechatconfig.Value.appsecret;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get(string code)
+        {
+            string verifyurl = wechatconfig.Value.verifyurl.Replace("paramer0",appid).Replace("paramer1",appsecret).Replace("paramer2",code);
+
+            return new string[] {verifyurl };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ api/api/Model/WechatConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model
+{
+    public class WechatConfig
+    {
+        public string appid { get; set; }
+        public string appsecret { get; set; }
+        public string verifyurl { get; set; }
+    }
+}
