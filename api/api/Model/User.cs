a57b29080035bba9473e60fae3d1d44f356547da
段兵宇 <duanbingyu@52yunxi.cn>
2018-06-22 18:01:08 +0800

    tz

---
+++ api/api/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model
+{
+    public class User
+    {
+        public int userid { get; set; }
+        public string username { set; get; }
+        public string password { get; set; }
+    }
+}
